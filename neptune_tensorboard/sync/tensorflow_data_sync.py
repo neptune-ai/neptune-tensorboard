2896e923cd3d71dd7914354224e01f0feae9cdbc
lukasz.walkiewicz <lukasz.walkiewicz@neptune.ml>
2019-08-26 16:30:46 +0200

    Don't upload an empty tensorflow graph (#19)

--- neptune_tensorboard/sync/tensorflow_data_sync.py
+++ neptune_tensorboard/sync/tensorflow_data_sync.py
@@ -81,7 +81,7 @@ class TensorflowDataSync(object):
     def _load_single_file(experiment, path, tf_integrator):
         tag_buckets = {}
         for record in tf.train.summary_iterator(path):
-            if record.graph_def != "":
+            if record.graph_def:
                 graph_def = tf.GraphDef()
                 graph_def.ParseFromString(record.graph_def)
                 tf_integrator.add_graph_def(graph_def, path)
