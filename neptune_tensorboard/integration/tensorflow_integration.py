3313c30e804e0af8aa1d42024f5c6a7046ca2872
Hubert Jaworski <hubert.jaworski@neptune.ml>
2020-03-26 14:48:50 +0100

    Allow to prefix channel names with log diectory name

--- neptune_tensorboard/integration/tensorflow_integration.py
+++ neptune_tensorboard/integration/tensorflow_integration.py
@@ -18,6 +18,7 @@ from __future__ import unicode_literals
 import io
 import os
 import time
+import sys
 
 import tensorflow as tf
 from PIL import Image
@@ -32,21 +33,32 @@ except Exception as ignore:
 _integrated_with_tensorflow = False
 
 
-def integrate_with_tensorflow(experiment_getter):
+def integrate_with_tensorflow(experiment_getter, prefix=False):
     global _integrated_with_tensorflow  # pylint:disable=global-statement
 
     if _integrated_with_tensorflow:
         return
-    _integrate_with_tensorflow(experiment_getter)
+    _integrate_with_tensorflow(experiment_getter, prefix)
     _integrated_with_tensorflow = True
 
 
 class TensorflowIntegrator(object):
 
-    def __init__(self, experiment_getter=None):
+    def __init__(self, prefix=False, experiment_getter=None):
         self._experiment_holder = experiment_getter
+        self._prefix = prefix
 
-    def add_summary(self, summary, global_step=None):
+    # pylint:disable=protected-access
+    def get_channel_name(self, writer, name):
+        if self._prefix:
+            if writer is not None:
+                log = writer.event_writer._logdir
+                writer_name = os.path.split(log)[1]
+                if writer_name:
+                    return writer_name + "_" + name
+        return name
+
+    def add_summary(self, writer, summary, global_step=None):
 
         if isinstance(summary, bytes):
             summ = summary_pb2.Summary()
@@ -56,22 +68,22 @@ class TensorflowIntegrator(object):
         x = self._calculate_x_value(global_step)
         for value in summary.value:
             try:
-                self.add_value(x, value)
+                self.add_value(x, value, writer)
             except NeptuneException:
                 pass
 
-    def add_value(self, x, value):
+    def add_value(self, x, value, writer):
         field = value.WhichOneof('value')
 
         if field == 'simple_value':
-            self.send_numeric(tag=value.tag,
+            self.send_numeric(tag=self.get_channel_name(writer, value.tag),
                               step=x,
                               value=value.simple_value,
                               wall_time=time.time())
             return
 
         if field == 'image':
-            self.send_image(tag=value.tag,
+            self.send_image(tag=self.get_channel_name(writer, value.tag),
                             step=x,
                             encoded_image_string=value.image.encoded_image_string,
                             wall_time=time.time())
@@ -83,7 +95,7 @@ class TensorflowIntegrator(object):
                 string_value = value.tensor.string_val.pop()
                 string_values.append(string_value.decode("utf-8"))
 
-            self.send_text(tag=value.tag,
+            self.send_text(tag=self.get_channel_name(writer, value.tag),
                            step=x,
                            text=", ".join(string_values),
                            wall_time=time.time())
@@ -122,8 +134,8 @@ class TensorflowIntegrator(object):
             return time.time()
 
 
-def _integrate_with_tensorflow(experiment_getter):
-    tensorflow_integrator = TensorflowIntegrator(experiment_getter=experiment_getter)
+def _integrate_with_tensorflow(experiment_getter, prefix=False):
+    tensorflow_integrator = TensorflowIntegrator(experiment_getter=experiment_getter, prefix=prefix)
 
     try:
         # noinspection PyUnresolvedReferences
@@ -131,7 +143,7 @@ def _integrate_with_tensorflow(experiment_getter):
 
         # Tensorflow 2.x
         if version >= parse_version('2.0.0-rc0'):
-            return _patch_tensorflow_2x(experiment_getter)
+            return _patch_tensorflow_2x(experiment_getter, prefix)
 
         # Tensorflow 1.x
         if version >= parse_version('1.0.0'):
@@ -148,7 +160,7 @@ def _patch_tensorflow_1x(tensorflow_integrator):
     _add_summary_method = tf.summary.FileWriter.add_summary
 
     def _neptune_add_summary(summary_writer, summary, global_step=None):
-        tensorflow_integrator.add_summary(summary, global_step)
+        tensorflow_integrator.add_summary(summary_writer, summary, global_step)
         _add_summary_method(summary_writer, summary, global_step=global_step)
 
     tf.summary.FileWriter.add_summary = _neptune_add_summary
@@ -157,7 +169,26 @@ def _patch_tensorflow_1x(tensorflow_integrator):
     return tensorflow_integrator
 
 
-def _patch_tensorflow_2x(experiment_getter):
+def _patch_tensorflow_2x(experiment_getter, prefix):
+    import tensorflow_core.python.ops.summary_ops_v2 as summary_ops_v2
+
+    # pylint:disable=protected-access
+    def get_channel_name(name):
+        if prefix:
+            context = summary_ops_v2.context.context()
+            if hasattr(context, 'summary_writer'):
+                writer = context.summary_writer
+            elif 'tensorflow.python.ops.summary_ops_v2' in sys.modules:
+                writer = sys.modules['tensorflow.python.ops.summary_ops_v2']._summary_state.writer
+            else:
+                writer = None
+            if writer is not None:
+                log = str(writer._init_op_fn.keywords['logdir']._numpy(), "utf-8")
+                writer_name = os.path.split(log)[1]
+                if writer_name:
+                    return writer_name + "_" + name
+        return name
+
     _scalar = tf.summary.scalar
     _text = tf.summary.text
     _image = tf.summary.image
@@ -165,19 +196,19 @@ def _patch_tensorflow_2x(experiment_getter):
     def scalar(name, data, step=None, description=None):
         if step is None:
             step = tf.summary.experimental.get_step()
-        experiment_getter().log_metric(name, x=step, y=data)
+        experiment_getter().log_metric(get_channel_name(name), x=step, y=data)
         _scalar(name, data, step, description)
 
     def image(name, data, step=None, max_outputs=3, description=None):
         if step is None:
             step = tf.summary.experimental.get_step()
-        experiment_getter().log_image(name, x=step, y=data, description=description)
+        experiment_getter().log_image(get_channel_name(name), x=step, y=data, description=description)
         _image(name, data, step, max_outputs, description)
 
     def text(name, data, step=None, description=None):
         if step is None:
             step = tf.summary.experimental.get_step()
-        experiment_getter().log_text(name, x=step, y=data)
+        experiment_getter().log_text(get_channel_name(name), x=step, y=data)
         _text(name, data, step, description)
 
     tf.summary.scalar = scalar
@@ -188,3 +219,17 @@ def _patch_tensorflow_2x(experiment_getter):
 
     tf.summary.text = text
     tf.summary._original_no_neptune_text = _text
+
+    _tb_log_metrics = tf.keras.callbacks.TensorBoard._log_metrics
+
+    def _log_metrics(instance, logs, prefix, step):
+        print(logs)
+        exp = experiment_getter()
+        for (name, value) in logs.items():
+            if name in ('batch', 'size', 'num_steps'):
+                continue
+            exp.log_metric(get_channel_name(name), x=step, y=value)
+
+        _tb_log_metrics(instance, logs, prefix, step)
+
+    tf.keras.callbacks.TensorBoard._log_metrics = _log_metrics
