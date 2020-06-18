cc74d6b2d0878525b756e9ac524e0b284fd898b2
Piotr Jander <pjander2@gmail.com>
2020-06-18 15:54:13 +0200

    Adds a deprecation warning for integrate_with_keras. (#35)
    Adds a deprecation warning for integrate_with_keras

--- neptune_tensorboard/integration/keras_integration.py
+++ neptune_tensorboard/integration/keras_integration.py
@@ -13,11 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 from neptune.exceptions import LibraryNotInstalled, NeptuneException
+
+from neptune_tensorboard.internal.deprecations import deprecated
 
 _integrated_with_keras = False
 
-
+@deprecated(
+    'This method is deprecated and does not work with Keras imported from Tensorflow'
+    'Please use Keras integration from the `neptune-contrib` package instead')
 def integrate_with_keras(experiment_getter):
     global _integrated_with_keras  # pylint:disable=global-statement
 
